de9cd896d2bb2ee87316211a175173ac9176e4b3
guneysrhat <guneysrhat@gmail.com>
2022-11-25 13:24:04 +0300

    table check-box

--- src/component/UserTable.jsx
+++ src/component/UserTable.jsx
@@ -1,8 +1,17 @@
-import { Checkbox, TableCell, TableRow } from "@mui/material";
+import {
+  Checkbox,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import * as React from "react";
 import { useState } from "react";
 
-const UserTable = ({ item }) => {
+const UserTable = ({ users }) => {
   const [isChecked, setIsChecked] = useState([]);
   const handleCheckBox = (e) => {
     const { id, value, checked } = e.target;
@@ -16,31 +25,58 @@ const UserTable = ({ item }) => {
   };
 
   return (
-    <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-      <TableCell align="center">
-        <Checkbox
-          id={item?.id}
-          value={item?.address?.geo?.lat}
-          
-          checked={item.isChecked}
-          onChange={(e) => handleCheckBox(e)}
-        />
-      </TableCell>
-      <TableCell align="center" component="th" scope="row">
-        {item?.id}
-      </TableCell>
-      <TableCell align="center">{item?.username}</TableCell>
+    <TableContainer component={Paper} sx={{ mt: 3 }} elevation={10}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">
+              <Checkbox />
+            </TableCell>
+            <TableCell align="center">#</TableCell>
+            <TableCell align="center">username</TableCell>
+            <TableCell align="center">name</TableCell>
+            <TableCell align="center">email</TableCell>
+            <TableCell align="center">phone</TableCell>
+            <TableCell align="center">website</TableCell>
+            <TableCell align="center">company</TableCell>
+            <TableCell align="center">address</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users?.map((item) => (
+            <TableRow
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              key={item.id}
+            >
+              <TableCell align="center">
+                <Checkbox
+                  id={item?.id}
+                  value={item?.address?.geo?.lat}
+                  checked={item.isChecked}
+                  onChange={(e) => handleCheckBox(e)}
+                />
+              </TableCell>
+              <TableCell align="center" component="th" scope="row">
+                {item?.id}
+              </TableCell>
+              <TableCell align="center">{item?.username}</TableCell>
 
-      <TableCell align="center">{item?.name}</TableCell>
-      <TableCell align="center">{item?.email}</TableCell>
-      <TableCell align="center">{item?.phone}</TableCell>
-      <TableCell align="center">{item?.website}</TableCell>
-      <TableCell align="center">{item?.company?.name}</TableCell>
-      <TableCell align="center">
-        {item?.address?.street} {item?.address?.suite} {item?.address?.city}
-        {item?.address?.zipcode}
-      </TableCell>
-    </TableRow>
+              <TableCell align="center">{item?.name}</TableCell>
+              <TableCell align="center">{item?.email}</TableCell>
+              <TableCell align="center">{item?.phone}</TableCell>
+              <TableCell align="center">{item?.website}</TableCell>
+              <TableCell align="center">{item?.company?.name}</TableCell>
+              <TableCell align="center">
+                {item?.address?.street}
+                {item?.address?.suite}
+                {item?.address?.city}
+                {item?.address?.zipcode}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 export default UserTable;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,7 @@ import React, { useEffect } from "react";
 import useUserCalls from "../hooks/useUserCalls";
 import UserTable from "../component/UserTable";
 import { useSelector } from "react-redux";
-import {
-  Box,
-  Checkbox,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const Home = () => {
   const { getUsers, getPosts } = useUserCalls();
@@ -30,30 +19,7 @@ const Home = () => {
           User Table
         </Typography>
 
-        <TableContainer component={Paper} sx={{ mt: 3 }} elevation={10}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">
-                  <Checkbox />
-                </TableCell>
-                <TableCell align="center">#</TableCell>
-                <TableCell align="center">username</TableCell>
-                <TableCell align="center">name</TableCell>
-                <TableCell align="center">email</TableCell>
-                <TableCell align="center">phone</TableCell>
-                <TableCell align="center">website</TableCell>
-                <TableCell align="center">company</TableCell>
-                <TableCell align="center">address</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {users?.map((item) => (
-                <UserTable key={item.id} item={item} />
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <UserTable users={users} />
       </Box>
     </>
   );
